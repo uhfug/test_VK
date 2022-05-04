43d8e07ea746162c7cf1e6b0d0fb0dede8db59c7
KonoDioDa <kirill_ternovskii@mail.ru>
2022-05-04 16:01:27 +0500

    vk_homework_1

---
+++ Test_VK/Test_VK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_VK
+//
+//  Created by Кирилл Терновский on 01.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
